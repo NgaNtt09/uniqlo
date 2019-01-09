72e179fe9f3fc913983f3f1409d6f8ba92ce8bb5
NgaNTT <nguyenthithuynga1420@gmail.com>
2019-01-09 15:30:26 +0700

    import dataproduct

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit,Input } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { ShopService } from '../shop.service';
+import { Product } from '../product';
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss']
 })
 export class ProductComponent implements OnInit {
-@Input() title:string;
-  constructor() { }
-
-  ngOnInit() {
+  @Input() title: string;
+  constructor(private shopService: ShopService) { }
+  products: Product[];
+  getProducts(): void {
+    this.shopService.getProducts().subscribe(products => this.products = products);
   }
+  ngOnInit() {
+    this.getProducts();
+  }
+
 
 }
--- src/app/shop.service.ts
+++ src/app/shop.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Titles } from './title-products';
 import { Observable , of } from 'rxjs';
-
+import { Products } from './products';
+import { Product } from './product';
 @Injectable({
   providedIn: 'root'
 })
@@ -10,5 +11,8 @@ export class ShopService {
   constructor() { }
   getTitles():Observable<string[]>{
     return of(Titles);
+  }
+  getProducts():Observable<Product[]>{
+    return of (Products);
   }
 }
