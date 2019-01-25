ef5d9bb50bb253d2331619d857b0e73b37b55910
NgaNTT <nguyenthithuynga1420@gmail.com>
2019-01-25 16:31:33 +0700

    feat(DB) config firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: true,
+  fisebase: {
+    apiKey: "AIzaSyAour2TchgJcdTja_Gy1KzAZlFgdfaib2M",
+    authDomain: "shopw-b3f7b.firebaseapp.com",
+    databaseURL: "https://shopw-b3f7b.firebaseio.com",
+    projectId: "shopw-b3f7b",
+    storageBucket: "shopw-b3f7b.appspot.com",
+    messagingSenderId: "118596433063"
+ 
+  }
+  
 };
 
 /*
