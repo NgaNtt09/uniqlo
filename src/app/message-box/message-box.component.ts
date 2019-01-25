0f8db7decb9c69038232c20abeb84603b62567b8
NgaNTT <nguyenthithuynga1420@gmail.com>
2019-01-25 16:33:27 +0700

    feat(add) message component

--- src/app/message-box/message-box.component.ts
+++ src/app/message-box/message-box.component.ts
@@ -1,14 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, Input } from '@angular/core';
 
 @Component({
   selector: 'app-message-box',
   templateUrl: './message-box.component.html',
-  styleUrls: ['./message-box.component.scss']
+  styleUrls: ['./message-box.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+  host:{
+    '[class.container]':"true",
+    '[class.container--rep]':"btnType === 'rep'",
+
+  }
 })
 export class MessageBoxComponent implements OnInit {
-
+@Input() btnType;
   constructor() { }
-
+  @Input() show:boolean=false;
   ngOnInit() {
   }
 
