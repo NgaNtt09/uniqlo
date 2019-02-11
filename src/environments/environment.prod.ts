9debc3e0420dc491a9c7dcac027fda3c3110a8c6
NgaNTT <nguyenthithuynga1420@gmail.com>
2019-02-11 09:37:31 +0700

    feat(firebase) setup environment of firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  fisebase: {
+    apiKey: "AIzaSyAour2TchgJcdTja_Gy1KzAZlFgdfaib2M",
+    authDomain: "shopw-b3f7b.firebaseapp.com",
+    databaseURL: "https://shopw-b3f7b.firebaseio.com",
+    projectId: "shopw-b3f7b",
+    storageBucket: "shopw-b3f7b.appspot.com",
+    messagingSenderId: "118596433063"
+ 
+  }
 };
