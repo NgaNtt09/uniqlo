a11410778aead1ab30b0524bcb730fb72e9c2eb0
NgaNTT <nguyenthithuynga1420@gmail.com>
2019-02-11 15:29:08 +0700

    Add @Input()

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,11 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
+  encapsulation:ViewEncapsulation.None,
+  host:{
+    '[class.container--img]':"img===true",
+  }
 })
 export class FooterComponent implements OnInit {
+
+  @Input() header: string;
+  @Input() container: string;
+  @Input() img=false;
 
   constructor() { }
 
