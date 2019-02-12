dab8746c52499c35ecd2d5a1608bc5371ac281d5
NgaNTT <nguyenthithuynga1420@gmail.com>
2019-02-12 15:13:36 +0700

    commit

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -6,14 +6,15 @@ import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./footer.component.scss'],
   encapsulation:ViewEncapsulation.None,
   host:{
-    '[class.container--img]':"img===true",
+    '[class.container--img]':"img==='true'",
+    '[class.container]':"true",
   }
 })
 export class FooterComponent implements OnInit {
 
   @Input() header: string;
   @Input() container: string;
-  @Input() img=false;
+  @Input() img;
 
   constructor() { }
 
